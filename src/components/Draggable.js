6629d4fa0ec2c4a5b7a1d1a99c79ac614bfd7a1d
1dawu2 <94387071+1dawu2@users.noreply.github.com>
2023-06-19 17:54:01 +0200

    update

--- src/components/Draggable.js
+++ src/components/Draggable.js
@@ -1,7 +1,53 @@
 let _shadowRoot;
 let tmpl = document.createElement("template");
 tmpl.innerHTML = `
-
+    <style>
+    </style>
+    <div id="ifm_hack_content" name="ifm_hack_content">
+      <slot name="content"></slot>
+    </div>
+    <script id="oView" name="oView" type="sapui5/xmlview">
+    <mvc:View
+      controllerName="ifm.drag.initial"
+	  xmlns:grid="sap.ui.layout.cssgrid"
+	  xmlns:dnd="sap.ui.core.dnd"
+	  xmlns:dnd-grid="sap.f.dnd"
+      xmlns:core="sap.ui.core"
+      xmlns:t="sap.ui.table"
+      xmlns:m="sap.m"
+      xmlns:f="sap.f"
+      xmlns:mvc="sap.ui.core.mvc">
+	    <Panel id="panelForGridList" backgroundDesign="Transparent">
+		<headerToolbar>
+			<Toolbar height="3rem">
+				<Title text="Grid List with Drag and Drop"/>
+			</Toolbar>
+		</headerToolbar>
+		<f:GridList
+			id="gridList"
+			headerText="GridList header">
+			<f:dragDropConfig>
+				<dnd:DragInfo sourceAggregation="items" />
+				<dnd-grid:GridDropInfo targetAggregation="items" dropPosition="Between" dropLayout="Horizontal" drop="onDrop" />
+			</f:dragDropConfig>
+			<f:customLayout>
+				<grid:GridBoxLayout boxMinWidth="17rem" />
+			</f:customLayout>
+			<f:GridListItem>
+				<VBox height="100%">
+					<VBox class="sapUiSmallMargin">
+						<layoutData>
+							<FlexItemData growFactor="1" shrinkFactor="0" />
+						</layoutData>
+						<Title text="Title" wrapping="true" />
+						<Label text="Subtitle" wrapping="true" />
+					</VBox>
+				</VBox>
+			</f:GridListItem>
+		</f:GridList>
+	    </Panel>
+    </mvc:View>
+    </script>
 `
 export default class IFMDraggable extends HTMLElement {
     constructor() {
@@ -71,7 +117,7 @@ export default class IFMDraggable extends HTMLElement {
             function (Controller) {
                 "use strict";
 
-                return Controller.extend("ifm.hack.initial", {
+                return Controller.extend("ifm.drag.initial", {
 
                 });
             });
