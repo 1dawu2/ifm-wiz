9437d2076fb9db99962f6935fcdf32d4a12e7e16
1dawu2 <94387071+1dawu2@users.noreply.github.com>
2023-06-20 07:41:30 +0200

    update

--- src/components/Draggable.js
+++ src/components/Draggable.js
@@ -18,15 +18,28 @@ tmpl.innerHTML = `
       xmlns:f="sap.f"
       xmlns:mvc="sap.ui.core.mvc">
       <m:Panel height="100%" expandable="true" expanded="true" headerText="SAC artifacts" id="oPanel">
-      	<m:List
-		headerText="Products"
-		items="{
-			path: '/ProductCollection'
-		}" >
-		<m:StandardListItem
-			title="{ProductID}"
-			counter="{Count}"/>
-	    </m:List>
+        <f:GridContainer
+            id="grid1"
+            snapToRow="true">
+            <f:layout>
+                <f:GridContainerSettings rowSize="5rem" columnSize="5rem" gap="1rem" />
+            </f:layout>
+            <f:Card width="400px">
+                <f:header>
+                <card:Header iconSrc="sap-icon://sort" title="Sort Order" subtitle="Material" />
+                </f:header>
+                <f:content>
+                <m:List
+                    showSeparators="None"
+                    items="{products>/productItems}">
+                    <m:StandardListItem
+                    description="{products>subtitle}"
+                    icon="{products>iconFile}"
+                    title="{products>title}" />
+                </m:List>
+                </f:content>
+            </f:Card>
+            </f:GridContainer>
       </m:Panel>
     </mvc:View>
     </script>
@@ -101,6 +114,71 @@ export default class IFMDraggable extends HTMLElement {
 
                 return Controller.extend("ifm.drag.initial", {
 
+
+                    onInit: function (oEvent) {
+                        this.oPanel = this.byId("oPanel");
+                        this.configGrid();
+                    },
+
+                    configGrid: function () {
+                        var DropLayout = sap.ui.core.dnd.DropLayout;
+                        var DropPosition = sap.ui.core.dnd.DropPosition;
+                        var oGrid = this.byId("grid1");
+                        var modelProduct = new sap.ui.model.json.JSONModel();
+                        modelProduct.setData(
+                            {
+                                "productItems": [
+                                    {
+                                        "title": "Website",
+                                        "subtitle": "http://www.infomotion.de",
+                                        "iconFile": "sap-icon://world"
+                                    },
+                                    {
+                                        "title": "Telefon",
+                                        "subtitle": "+49 69 56608 3231",
+                                        "iconFile": "sap-icon://call"
+                                    },
+                                    {
+                                        "title": "Mail",
+                                        "subtitle": "david.wurm@infomotion.de",
+                                        "iconFile": "sap-icon://business-card"
+                                    }
+                                ]
+                            }
+                        );
+                        sap.ui.getCore().setModel(modelProduct, "products");
+
+                        oGrid.addDragDropConfig(new sap.ui.core.dnd.DragInfo({
+                            sourceAggregation: "items"
+                        }));
+
+                        oGrid.addDragDropConfig(new sap.f.dnd.GridDropInfo({
+                            targetAggregation: "items",
+                            dropPosition: DropPosition.Between,
+                            dropLayout: DropLayout.Horizontal,
+                            drop: function (oInfo) {
+                                var oDragged = oInfo.getParameter("draggedControl"),
+                                    oDropped = oInfo.getParameter("droppedControl"),
+                                    sInsertPosition = oInfo.getParameter("dropPosition"),
+                                    iDragPosition = oGrid.indexOfItem(oDragged),
+                                    iDropPosition = oGrid.indexOfItem(oDropped);
+
+                                oGrid.removeItem(oDragged);
+
+                                if (iDragPosition < iDropPosition) {
+                                    iDropPosition--;
+                                }
+
+                                if (sInsertPosition === "After") {
+                                    iDropPosition++;
+                                }
+
+                                oGrid.insertItem(oDragged, iDropPosition);
+                                oGrid.focusItem(iDropPosition);
+                            }
+                        }));
+                    },
+
                 });
             });
 
@@ -111,7 +189,4 @@ export default class IFMDraggable extends HTMLElement {
         oView.placeAt(content);
 
     }
-
-
-
 }
